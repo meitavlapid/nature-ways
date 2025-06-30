7e0a074e0ca614f78ef931839afa392dfa2f0100
meitavlapid <meitavlapid@gmail.com>
2025-06-30 15:07:28 +0300

    home

--- front/components/Home.jsx
+++ front/components/Home.jsx
@@ -43,19 +43,39 @@ function Home() {
     <div className="container">
       {/* תמונה ראשית */}
       {getImageUrl("home") && (
-        <img src={getImageUrl("home")} alt="תמונה ראשית" />
+        <img
+          src={getImageUrl("home")}
+          alt="תמונה ראשית"
+          className="main-image"
+        />
       )}
-
       {/* שתי תמונות נוספות */}
       <div className="image-row">
         {getImageUrl("nutri") && (
-          <img src={getImageUrl("nutri")} alt="תמונה 2" />
+          <div className="flip-card">
+            <div className="flip-inner">
+              <div className="flip-front">
+                <img src={getImageUrl("nutri")} alt="תמונה 2" />
+              </div>
+              <div className="flip-back">
+                <span>נוטריקוסמטיקה</span>
+              </div>
+            </div>
+          </div>
         )}
         {getImageUrl("dermo") && (
-          <img src={getImageUrl("dermo")} alt="תמונה 3" />
+          <div className="flip-card">
+            <div className="flip-inner">
+              <div className="flip-front">
+                <img src={getImageUrl("dermo")} alt="תמונה 3" />
+              </div>
+              <div className="flip-back">
+                <span>דרמוטולוגיה</span>
+              </div>
+            </div>
+          </div>
         )}
       </div>
-
       <div className="text-container">
         <p>עם הזמן הצטברו לא רק מוצרים – גם סיפורים.</p>
         <p>
@@ -63,20 +83,21 @@ function Home() {
           אותנטיים שממחישים איך נייצ'ר וויז משתלבת ביום-יום של קליניקות,
           מטפלים ומטופלים.
         </p>
-    
-      {/* קרוסלת סרטונים */}
-      {videos.length > 0 && (
-        <div className="video-carousel">
-          <Slider {...settings}>
-            {videos.map((video) => (
-              <div className="video-slide" key={video._id}>
-                <video controls src={video.url} width="100%" height="400" />
-              </div>
-            ))}
-          </Slider>
-        </div>
-      )}
-    </div>  </div>
+
+        {/* קרוסלת סרטונים */}
+        {videos.length > 0 && (
+          <div className="video-carousel">
+            <Slider {...settings}>
+              {videos.map((video) => (
+                <div className="video-slide" key={video._id}>
+                  <video controls src={video.url} width="100%" height="400" />
+                </div>
+              ))}
+            </Slider>
+          </div>
+        )}
+      </div>{" "}
+    </div>
   );
 }
 
