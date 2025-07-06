1ca745078c5f90db6f7f4fc4637542034cfb61ab
meitavlapid <meitavlapid@gmail.com>
2025-07-06 15:43:35 +0300

    111

--- front/components/ProductListByCategory.jsx
+++ front/components/ProductListByCategory.jsx
@@ -69,7 +69,7 @@ function ProductListByCategory() {
       {products.length === 0 ? (
         <p className="text-center">לא נמצאו מוצרים בקטגוריה זו.</p>
       ) : (
-        <div className="row mt-4 mb-4">
+        <div className="row row-cols-1 row-cols-md-2 g-4">
           {products.map((product) => (
             <div className="col" key={product._id}>
               <div className="card-product">
